5d5558a9d8e1b76be779e2a9c9c0206509b4d38f
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-01-10 23:36:13 +0900

    Docs: edited gitignore

--- sharenow_frontend/src/container/Home.jsx
+++ sharenow_frontend/src/container/Home.jsx
@@ -3,6 +3,9 @@ import { HiMenu } from "react-icons/hi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
 
+import Sidebar from "../components/Sidebar";
+import UserProfile from "../components/UserProfile";
+
 const Home = () => {
   return <div>Home</div>;
 };
