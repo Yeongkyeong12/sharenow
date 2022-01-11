9bd03f9e4ba5a84a39dc3020042f639483b95e89
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-01-11 23:15:07 +0900

    Feat: add a click function for the sidebar

--- sharenow_frontend/src/container/Home.jsx
+++ sharenow_frontend/src/container/Home.jsx
@@ -27,13 +27,13 @@ const Home = () => {
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
-        <Sidebar />
+        <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row">
         <HiMenu
           fontSize={40}
           className="cursor-pointer"
-          onClick={() => setToggleSidebar(false)}
+          onClick={() => setToggleSidebar(true)}
         />
         <Link to="/">
           <img src={logo} alt="logo" className="w-28"></img>
@@ -42,6 +42,18 @@ const Home = () => {
           <img src={user?.image} alt="logo" className="w-28"></img>
         </Link>
       </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(false)}
+            />
+          </div>
+          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        </div>
+      )}
     </div>
   );
 };
