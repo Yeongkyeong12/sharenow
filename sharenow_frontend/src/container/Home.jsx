8c3b6c11bad430fa2de9e361c879d0135d627ce3
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-01-11 23:05:16 +0900

    Feat: add link component

--- sharenow_frontend/src/container/Home.jsx
+++ sharenow_frontend/src/container/Home.jsx
@@ -5,11 +5,45 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { Sidebar, UserProfile } from "../components";
 import Pins from "./Pins";
+import { userQuery } from "../utils/data";
 import { client } from "../client";
 import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <div className="flex bg-gray-50">Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28"></img>
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="logo" className="w-28"></img>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
